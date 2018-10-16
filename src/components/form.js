622c94862e49369aa583bc63cd20d2e10084a89a
Sina Burow <sina.burow@sinnerschrader.com>
2018-10-16 14:40:55 +0200

    styled form

--- src/components/form.js
+++ src/components/form.js
@@ -1,4 +1,92 @@
 import React from 'react'
+import styled from 'react-emotion'
+
+const StyledForm = styled('form')`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-size: 16px;
+`;
+
+const StyledFormWrapper = styled('div')`
+    margin-top: 50px;
+`;
+
+const StyledSpan = styled('span')`
+    margin: 0;
+    display: inline-block;
+    position: relative;
+    vertical-align: top;
+    margin-left: 10px;
+    margin-right: 10px;
+    width: 85px;
+
+    input[type="radio"] {
+        display:none;
+    }
+
+    input[type="radio"] + label::before {
+  transition: all 250ms cubic-bezier(.4,.25,.3,1);
+  content: "";
+  width: 15px;
+  height: 15px;
+  border-radius: 50%;
+  background-color: white;
+  border: 0px solid white;
+  font-size: 0;
+  position: absolute;
+  bottom: 145%;
+  left: 50%;
+  transform: translate(-50%, 50%);
+}
+
+input[type="radio"] + label::after {
+  transition: all 250ms cubic-bezier(.4,.25,.3,1);
+  content: "";
+  width: 0;
+  height: 0;
+  border-radius: 50%;
+  background-color: white;
+  position: absolute;
+  bottom: 145%;
+  left: 50%;
+  transform: translate(-50%, 50%);
+}
+
+input[type="radio"]:checked + label::after {
+  width: 15px;
+  height: 15px;
+}
+
+input[type="radio"]:checked + label::before {
+  background-color: transparent;
+  width: 25px;
+  height: 25px;
+  border-width: 2px;
+}
+`;
+
+const StyledButtonWrap = styled('div')`
+    margin: 50px 0;
+`;
+
+const StyledButton = styled('button')`
+  text-decoration: none;
+  width: 150px;
+  height: 150px;
+  color: #AD1457;
+  border: 1px solid #F5F5F5;
+  border-radius: 50%;
+  background-color: #F5F5F5;
+  box-shadow: 0px 9px 16px 5px rgba(0,0,0,0.6);
+  font-family: inherit;
+  font-size: inherit;
+  font-weight: inherit;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
 
 class Formular extends React.Component {
   constructor(props){
@@ -43,25 +131,25 @@ class Formular extends React.Component {
 
   render() {
     return (
-      <form> 
-        <section className="formular">
-            <p>
+      <StyledForm>
+        <StyledFormWrapper>
+            <StyledSpan>
                 <input type="radio" name="time" id="short" value="short" checked={this.state.selectedOption === 'short'} onChange={this.handleOptionChange}  />
                 <label htmlFor="short">asapst weiter&shy;arbeiten</label>
-            </p>
-            <p>
+            </StyledSpan>
+            <StyledSpan>
                 <input type="radio" name="time" id="medium" value="medium" checked={this.state.selectedOption === 'medium'} onChange={this.handleOptionChange} />
                 <label htmlFor="medium">normal, business as usual</label>
-            </p>
-            <p>
+            </StyledSpan>
+            <StyledSpan>
                 <input type="radio" name="time" id="long" value="long" checked={this.state.selectedOption === 'long'} onChange={this.handleOptionChange}  />
                 <label htmlFor="long">mal entspannt abloungen</label>
-            </p>
-        </section>
-        <section className="buttonWrap">
-          <button type="button" onClick={this.handleClick} value="Submit">Jetzt unbedingt<br />HIER KLICKEN</button>
-        </section>
-      </form>
+            </StyledSpan>
+        </StyledFormWrapper>
+        <StyledButtonWrap>
+          <StyledButton type="button" onClick={this.handleClick} value="Submit">Jetzt unbedingt<br />HIER KLICKEN</StyledButton>
+        </StyledButtonWrap>
+      </StyledForm>
     );
   }
 };
